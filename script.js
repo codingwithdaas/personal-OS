2afe89786217a91cabf20e8d8246375d0b1663ce
codingwithdaas <codingwithdaas@gmail.com>
2026-07-18 16:40:53 -0700

    welcome page -> closable/reopenable window, can be moved

--- script.js
+++ script.js
@@ -4,3 +4,63 @@ function updateTime() {
         timeText.innerHTML = currentTime;
     }
     setInterval(updateTime, 1000);
+
+dragElement(document.getElementById("welcome"));
+
+function dragElement(element) {
+    var initialX = 0;
+    var initialY = 0;
+    var currentX = 0;
+    var currentY = 0;
+
+    if (document.getElementById(element.id + "Header")) {
+        document.getElementById(element.id + "Header").onmousedown = startDragging;
+    } else {
+        element.onmousedown = startDragging;
+    }
+
+    function startDragging(e) {
+        e = e || window.event;
+        e.preventDefault();
+        initialX = e.clientX;
+        initialY = e.clientY;
+        document.onmouseup = stopDragging;
+        document.onmousemove = dragElement;
+    }
+
+    function dragElement(e) {
+        e = e || window.event;
+        e.preventDefault();
+        currentX = initialX - e.clientX;
+        currentY = initialY - e.clientY;
+        initialX = e.clientX;
+        initialY = e.clientY;
+        element.style.top = (element.offsetTop - currentY) + "px";
+        element.style.left = (element.offsetLeft - currentX) + "px";
+    }
+
+    function stopDragging() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
+
+function closeWindow(element) {
+    element.style.display = "none";
+}
+
+function openWindow(element) {
+    element.style.display = "flex";
+}
+
+var welcomeScreen = document.querySelector("#welcome");
+var welcomeScreenClose = document.querySelector("#welcomeclose");
+var welcomeScreenOpen = document.querySelector("#welcomeopen");
+
+welcomeScreenClose.addEventListener("click", function() {
+    closeWindow(welcomeScreen);
+});
+
+welcomeScreenOpen.addEventListener("click", function() {
+    openWindow(welcomeScreen);
+});
